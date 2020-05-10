6f80dc0e2f6f56e7326553df5d31ca0cdcb4bdf9
simesi <simesi@hotmail.it>
2020-05-10 16:20:17 +0200

    ddd

--- src/logic/MyHelloWorld.java
+++ src/logic/MyHelloWorld.java
@@ -1,37 +1,7 @@
-/**
- * Copyright (C) 2017 Simone Mesiano Laureani (a.k.a. Simesi)
- *    
- *    This file is part of the contents developed for the course
- * 	  ISPW (A.Y. 2017-2018) at Università di Tor Vergata in Rome.
- *
- *    This is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as 
- *    published by the Free Software Foundation, either version 3 of the 
- *    License, or (at your option) any later version.
- *
- *    This software is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with this source.  If not, see <http://www.gnu.org/licenses/>.
- */
 package src.logic;
 
-/**
- * @author simone
- *
- */
 public class MyHelloWorld {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Hello world!!");
-
+		System.out.println("Hello world!!");
 	}
-
 }
