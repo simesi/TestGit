8b5bbdfd50c476a840f02f7577ce441c29e1b8f0
Simone <39402200+simesi@users.noreply.github.com>
2020-11-05 18:40:48 +0100

    world 1 e world 2

--- src/logic/MyHelloWorld.java
+++ src/logic/MyHelloWorld.java
@@ -2,7 +2,7 @@ package src.logic;
 
 public class MyHelloWorld {
 	public static void  main(String[] args) {
-		System.out.println("Hello worlda!");
-		System.out.println("Hello world again"):
+		System.out.println("Hello world1!");
+		System.out.println("Hello world2 again"):
 	}
 }
