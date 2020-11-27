b997c85d1b808b3662022228927ebba478deac66
simesi <simesi@hotmail.it>
2020-11-27 17:47:01 +0100

    added a file and deleted a line on another files

--- src/logic/MyHelloWorld.java
+++ src/logic/MyHelloWorld.java
@@ -3,6 +3,5 @@ package src.logic;
 public class MyHelloWorld {
 	public static void  main(String[] args) {
 		System.out.println("Hello world1!");
-		System.out.println("Hello world2 again"):
 	}
 }
