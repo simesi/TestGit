c7c2d0653a51b0d447ef5f887f177e05212997dd
simesi <simesi@hotmail.it>
2020-12-01 10:19:06 +0100

    added a method and a variabile

--- src/logic/MyHelloWorld.java
+++ src/logic/MyHelloWorld.java
@@ -1,7 +1,11 @@
 package src.logic;
 
 public class MyHelloWorld {
+int i=0;
 	public static void  main(String[] args) {
+		System.out.println("Hello world!!");
+	}
+public static void myMethod(String args) {
 		System.out.println("Hello world!!");
 	}
 }
