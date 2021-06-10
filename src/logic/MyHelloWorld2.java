fd8f4deda26edbe4defd8cf38fc7cf367a2c442a
Simone <39402200+simesi@users.noreply.github.com>
2021-06-10 18:55:31 +0200

    Update MyHelloWorld2.java

--- src/logic/MyHelloWorld2.java
+++ src/logic/MyHelloWorld2.java
@@ -2,7 +2,7 @@ package src.logic;
 
 public class MyHelloWorld2 {
 	int i=2;
-	public static void  main(String[] args) {
+	public static void  test(String args) {
 		System.out.println("Hello world!");
 	}
 }
